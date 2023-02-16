a3b35c204e773290dba385ba14d38514565a566d
hasan <hasanakoz1986@gmail.com>
2023-02-16 21:48:18 +0300

    register forms done

--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .form import UserForm
 
 # Create your views here.
 
 def register(request):
-    form = UserCreationForm()
+    # form = UserCreationForm()
+    form = UserForm()
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+
 
     context = {
         "form" : form
