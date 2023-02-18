6a7fb95f506e1fbfd81a5061540dd94663ae03b1
hasan <hasanakoz1986@gmail.com>
2023-02-18 13:19:10 +0300

    order page done

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Pizza
+from django.shortcuts import render, redirect
+from .models import Pizza, Order
+from .forms import PizzaForm
 
 # Create your views here.
 
@@ -13,3 +14,28 @@ def pizzas(request):
         'pizzas' : pizzas
     }
     return render(request, 'pizzas/pizzas.html', context)
+
+def order_view(request, id):
+    pizza = Pizza.objects.get(id=id)
+    form = PizzaForm(request.POST or None)
+
+    if request.method == "POST" :
+        if form.is_valid():
+            order= form.save(commit=False)
+            order.pizza = pizza
+            order.user = request.user
+            order.save()
+            return redirect('home')
+
+    context = {
+        "pizza" : pizza,
+        "form" : form
+    }
+    return render(request, 'pizzas/order.html', context)
+
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user)
+    context = {
+        'orders': orders
+    }
+    return render(request, 'pizzas/my_orders.html', context)
