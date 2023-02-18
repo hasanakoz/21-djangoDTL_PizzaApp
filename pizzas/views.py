842c64ed7efd325abb50d865b3270a5ba41773d7
hasan <hasanakoz1986@gmail.com>
2023-02-18 12:25:55 +0300

    pizzas page done

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 
 def home(request):
     return render(request, 'pizzas/home.html')
+
+def pizzas(request):
+    pizzas = Pizza.objects.all()
+
+    context = {
+        'pizzas' : pizzas
+    }
+    return render(request, 'pizzas/pizzas.html', context)
